6698caad966db8c9b46474257efa3a6c66d02c61
Jo Fischer <josephinefischer9@gmail.com>
2020-06-11 11:43:39 -0400

    done

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -13,8 +13,42 @@ class TweetsController < ApplicationController
   end
 
   post "/tweets" do
-    Tweet.create(content: params[:content], user: Helpers.current_user(session))
+    if params[:content].empty?
+      redirect to "/tweets/new"
+    else
+      Tweet.create(content: params[:content], user: Helpers.current_user(session))
 
-    redirect to("/tweets")
+      redirect to("/tweets")
+    end
+  end
+
+  get "/tweets/:id" do
+    redirect to("/login") unless session[:user_id].present?
+    @tweet = Tweet.find(params[:id])
+    erb :"/tweets/show"
+  end
+
+  get "/tweets/:id/edit" do
+    redirect to("/login") unless session[:user_id].present?
+    @tweet = Tweet.find(params[:id])
+    erb :"/tweets/edit"
+  end
+
+  patch "/tweets/:id" do
+    if params[:content].empty?
+      redirect to "/tweets/#{params[:id]}/edit"
+    else
+      tweet = Tweet.find(params[:id])
+      tweet.update(content: params[:content])
+      redirect to "/tweets/#{tweet.id}"
+    end
+  end
+
+  delete "/tweets/:id" do
+    redirect to("/login") unless session[:user_id].present?
+    tweet = Tweet.find(params[:id])
+    redirect to "/tweets/#{tweet.id}" unless Helpers.current_user(session) == tweet.user
+    tweet.destroy
+    erb :"/tweets/index"
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,14 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
+
+  def slug
+    content.downcase.tr(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    all.find do |tweet|
+      tweet.slug == slug
+    end
+  end
+
 end
