bcc2a6fee309e3d41e9540337b55dd18a586612f
Pat McClernan <patmccler@gmail.com>
2020-06-11 10:45:15 -0400

    swapping drivers

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,20 @@
 class TweetsController < ApplicationController
 
+  get "/tweets" do
+    redirect to("/login") unless session[:user_id].present?
+    @tweets = Tweet.all
+    erb :"tweets/index"
+  end
 
+  get "/tweets/new" do
+    redirect to("/login") unless session[:user_id].present?
+
+    erb :"tweets/new"
+  end
+
+  post "/tweets" do
+    Tweet.create(content: params[:content], user: Helpers.current_user(session))
+
+    redirect to("/tweets")
+  end
 end
